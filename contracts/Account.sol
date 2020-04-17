975ac2ad69dd9201cb346b8de12e74a7cfa6bdd6
Isaac Lin <me@isaaclin.cn>
2020-04-17 16:36:47 +0800

    Finish registration, deposit, transfer and withdraw within contract

---
+++ contracts/Account.sol
@@ -0,0 +1,131 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Account {
+    /// User's information
+    struct User {
+        address bindAddress;
+        string accountID;
+        string password;
+        uint balance;
+        bool inGame;
+        bool registered;
+    }
+
+    /// Mapping from accountID to address.
+    mapping(string => address) internal accountToAddress;
+    /// Mapping from address to user's information.
+    mapping(address => User) internal userList;
+
+    /**
+     * A account will be created if both the address and the account ID is not taken.
+     * @param  accountID The account ID the user would like to use.
+     */
+    modifier notRegistered (string memory accountID) {
+        require (
+            !userList[msg.sender].registered && !userList[accountToAddress[accountID]].registered,
+            "The address/account ID have already registered!"
+        );
+        _;
+    }
+
+    /**
+     * Guarantee the msg.sender is a registered user.
+     */
+    modifier registered {
+        require (
+            userList[msg.sender].registered,
+            "Your address has not registered yet! Please check again."
+        );
+        _;
+    }
+
+    /**
+     * Guarantee the msg.sender has enough balance to do the balance-related activity.
+     */
+    modifier enoughBalance(uint amount) {
+        require (userList[msg.sender].balance > amount, "You do not have enough balance!");
+        _;
+    }
+
+    /**
+     * @param accountID The account ID the user would like to use.
+     */
+    function createAccount(string memory accountID) public notRegistered(accountID) {
+        //
+        accountToAddress[accountID] = msg.sender;
+        //
+        userList[msg.sender].accountID = accountID;
+        userList[msg.sender].bindAddress = msg.sender;
+        // userList[msg.sender].password =;
+        userList[msg.sender].balance = 0;
+        userList[msg.sender].inGame = false;
+        userList[msg.sender].registered = true;
+    }
+
+    /**
+     * User deposit the money to the contract, and the contract will give him a "virtual" balance, similar to deposit fiat money to bank.
+     * @return balance The balance of the user.
+     */
+    function deposit() external payable returns (uint balance) {
+        require (
+            userList[msg.sender].registered,
+            "The address has not registered yet! Please check again."
+        );
+
+        userList[msg.sender].balance += msg.value;
+
+        return userList[msg.sender].balance;
+    }
+
+    /**
+     * User transfer his/her deposit to another registered user with the user's address.
+     * @param toAddress The address of the target registered user to transfer balance to.
+     * @param amount The amount of balance to transfer to the target registered user.
+     * @return balance The balance of the user.
+     */
+    function transferToAddress(address toAddress, uint amount) external registered enoughBalance(amount) returns (uint balance) {
+        require (
+            userList[toAddress].registered,
+            "The target address have not registered yet! Please check again."
+        );
+        userList[msg.sender].balance -= amount;
+        userList[toAddress].balance += amount;
+
+        return userList[msg.sender].balance;
+    }
+
+    /**
+     * User transfer his/her deposit to another registered user with the user's accountID.
+     * @param  toAccountID The accountID of the target registered user to transfer balance to.
+     * @param amount The amount of balance to transfer to the target registered user.
+     * @return balance The balance of the user.
+     */
+    function transferToID(string memory toAccountID, uint amount) public registered enoughBalance(amount) returns (uint balance) {
+        require (
+            userList[accountToAddress[toAccountID]].registered,
+            "The target address have not registered yet! Please check again."
+        );
+        userList[msg.sender].balance -= amount;
+        userList[accountToAddress[toAccountID]].balance += amount;
+
+        return userList[msg.sender].balance;
+    }
+
+    /**
+     * Withdraw the balance from the contract, get the token back to his/her ethureum address.
+     * @param amount The amount of balance to withdraw.
+     */
+    function withdraw(uint amount) external payable registered enoughBalance(amount) returns (uint balance) {
+        userList[msg.sender].balance -= amount;
+        msg.sender.transfer(amount);
+
+        return userList[msg.sender].balance;
+    }
+
+    /**
+     * Show the balance.
+     */
+    function checkBalance() external view registered returns (uint balance) {
+        return userList[msg.sender].balance;
+    }
+}
