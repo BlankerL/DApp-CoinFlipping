c8570157e3157f31298bb0e168e2ed43feecbccf
Isaac Lin <me@isaaclin.cn>
2020-04-20 03:46:10 +0800

    Auto-load the address and auto-scaling the site

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,27 @@
-App = {
+coinFlipWeb3 = {
     web3Provider: null,
-    contracts: {},
 
     init: function() {
-        return App.initWeb3();
+        return this.initWeb3();
     },
 
     initWeb3: function() {
         // Initialize web3 and set the provider to the testRPC.
         if (typeof web3 !== 'undefined') {
             // For MetaMask, we need to call this to allow the site read the accounts.App.web3Provider = web3.currentProvider;
+            coinFlipWeb3.web3Provider = web3.currentProvider;
             web3 = new Web3(web3.currentProvider);
         } else {
-            App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
-            web3 = new Web3(App.web3Provider);
+            coinFlipWeb3.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+            web3 = new Web3(coinFlipWeb3.web3Provider);
         }
-        return true;
-    }
-};
+        return this.initContract();
+    },
 
-$(window).on('load', function () {
-    App.init();
-});
+    initContract: function() {
+        $.getJSON('CoinFlip.json', function(data) {
+            // Get the necessary contract artifact file and instantiate it with truffle-contract.
+            coinFlipWeb3.contract = new web3.eth.Contract(data['abi'], "0xD759Ac5344c0A9ecB2C738d45411Ffa967C95A91");
+        });
+    },
+};
