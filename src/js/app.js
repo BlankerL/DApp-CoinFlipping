dc06650fd489b7589e5dc4c97e0dddd275053081
Isaac Lin <me@isaaclin.cn>
2020-04-20 04:25:08 +0800

    Because JS will run without wait for the value comes up, we need to setTimeout to execute function

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 coinFlipWeb3 = {
     web3Provider: null,
+    contract: null,
 
     init: function() {
         return this.initWeb3();
@@ -9,19 +10,19 @@ coinFlipWeb3 = {
         // Initialize web3 and set the provider to the testRPC.
         if (typeof web3 !== 'undefined') {
             // For MetaMask, we need to call this to allow the site read the accounts.App.web3Provider = web3.currentProvider;
-            coinFlipWeb3.web3Provider = web3.currentProvider;
+            this.web3Provider = web3.currentProvider;
             web3 = new Web3(web3.currentProvider);
         } else {
-            coinFlipWeb3.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
-            web3 = new Web3(coinFlipWeb3.web3Provider);
+            this.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+            web3 = new Web3(this.web3Provider);
         }
         return this.initContract();
     },
 
     initContract: function() {
-        $.getJSON('CoinFlip.json', function(data) {
+        $.getJSON('contracts/CoinFlip.json', function(data) {
             // Get the necessary contract artifact file and instantiate it with truffle-contract.
-            coinFlipWeb3.contract = new web3.eth.Contract(data['abi'], "0xD759Ac5344c0A9ecB2C738d45411Ffa967C95A91");
+            this.contract = new web3.eth.Contract(data['abi'], "0xFF472b76B1a023c1dBD3e0d02ba460c417Fec4bf");
         });
     },
 };
