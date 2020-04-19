902af5b387abb605346cb1711420b5a0c59696af
Isaac Lin <me@isaaclin.cn>
2020-04-20 04:28:34 +0800

    Cannot use `this`

--- src/js/app.js
+++ src/js/app.js
@@ -3,26 +3,26 @@ coinFlipWeb3 = {
     contract: null,
 
     init: function() {
-        return this.initWeb3();
+        return coinFlipWeb3.initWeb3();
     },
 
     initWeb3: function() {
         // Initialize web3 and set the provider to the testRPC.
         if (typeof web3 !== 'undefined') {
             // For MetaMask, we need to call this to allow the site read the accounts.App.web3Provider = web3.currentProvider;
-            this.web3Provider = web3.currentProvider;
+            coinFlipWeb3.web3Provider = web3.currentProvider;
             web3 = new Web3(web3.currentProvider);
         } else {
-            this.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
-            web3 = new Web3(this.web3Provider);
+            coinFlipWeb3.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+            web3 = new Web3(coinFlipWeb3.web3Provider);
         }
-        return this.initContract();
+        return coinFlipWeb3.initContract();
     },
 
     initContract: function() {
         $.getJSON('contracts/CoinFlip.json', function(data) {
             // Get the necessary contract artifact file and instantiate it with truffle-contract.
-            this.contract = new web3.eth.Contract(data['abi'], "0xFF472b76B1a023c1dBD3e0d02ba460c417Fec4bf");
+            coinFlipWeb3.contract = new web3.eth.Contract(data['abi'], "0xFF472b76B1a023c1dBD3e0d02ba460c417Fec4bf");
         });
     },
 };
